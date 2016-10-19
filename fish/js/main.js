3644f09e7f595cf4e1e721c7c9ae184e5537498f
NasirQrn <349675161@qq.com>
2016-10-19 11:49:48 +0800

    Initial commit

---
+++ fish/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by hasee on 2016/10/19.
+ */
+//全局变量
+var can1,can2 =0;//画布
+var ctx1,ctx2;//场景
+document.body.onload = game;
+function game(){
+    init();
+}
+function init(){
+    //获得canvas context
+    console.log(0 == false);
+}
